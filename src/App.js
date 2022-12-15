037d74fc6e82dd233512e982a7a2d413a4f0dd14
peterheri <heriachanga@gmail.com>
2022-12-15 16:15:07 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
+import Modal from './components/Modals'
 
 //using stateHook
 import { useState } from 'react'; //importing use state from the react library
 function App() {
   // Outputting lists 
   const [showEvents, setShowEvents] = useState(true)
+  const [showModal, setShowModal] = useState(false)
   const [events, setEvents] = useState([
       {title:"Peter's birthday bash", id: 1},
       {title:"Play video games", id: 2},
       {title:"Go to the gym", id: 3}
   ])
 
-  console.log(showEvents)
+  console.log(showModal)
 
 
   const handleClick = (id) => {
@@ -21,12 +23,16 @@ function App() {
           return id !== event.id
         })
       })
-   //retutns a new array that has been filtered from the orginal one
-    console.log(id)
-
-   
     
   }
+
+  const handleClose = () => {
+    setShowModal(false)
+  }
+
+ 
+
+ 
 
   return (
     <div className="App">
@@ -39,12 +45,21 @@ function App() {
           <button onClick={() => setShowEvents(true)}>Shows Events</button>
           </div>
         )}
+
+
+        <button onClick={()=> setShowModal(true)}>Show Modal</button>
     {showEvents && events.map((event) => (
       <div key={event.id}> 
         <h2>{event.title}</h2>
         <button onClick = {() => handleClick(event.id)}>delete event</button>
       </div>
     ))}
+    
+    {showModal && (<Modal handleClose = {handleClose}>
+        <h2>Terms and Conditions</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+        {/* <a href="#"> find out more</a> */}
+    </Modal>)}
     </div>
   );
 }
---
+++ src/components/Modals.js
@@ -0,0 +1,13 @@
+
+import './Modal.css'
+
+export default function Modal ({children, handleClose}) {
+  return (
+    <div className="modal-backdrop">
+      <div className="modal">
+        {children}
+        <button onClick={handleClose}>Close</button>
+      </div>
+    </div>
+  )
+}
